549330b0d3bd243f3f7b4cf79a66f01ef4ca4585
Daniel Franchini <73396382+dani3lfrenc@users.noreply.github.com>
2023-09-24 17:06:51 +0200

    feat: added method constructor

--- src/Tubo.java
+++ src/Tubo.java
@@ -1,4 +1,14 @@
 public class Tubo {
     private float raggio;
     private float altezza;
+
+    public Tubo (float raggio, float altezza) throws Exception{
+        if (raggio <= 0 || altezza <= 0 ) {
+            throw new Exception("Raggio o altezza non valido!");
+        }
+        this.raggio = raggio;
+        this.altezza = altezza;
+    }
+
+
 }
