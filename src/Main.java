513cd54a1e2e1798583fd117dc2815e8ea1350a7
Daniel Franchini <73396382+dani3lfrenc@users.noreply.github.com>
2023-09-27 18:54:41 +0200

    test: "tubo" class creation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner input = new Scanner(System.in);
+        Tubo tubo = null;
+        float raggio = 0;
+        float altezza = 0;
+        boolean flag = false;
+
+        do {
+            System.out.print("Inserire il raggio del tubo: ");
+            raggio = input.nextFloat();
+            System.out.print("\n\nInserire l'altezza delo tubo: ");
+            altezza = input.nextFloat();
+            try {
+                tubo = new Tubo(raggio,altezza);
+                flag = true;
+            } catch (Exception e) {
+                System.out.println("Valori non validi");
+            }
+        } while (!flag);
+
+
+
+
+
+
+
     }
 }
